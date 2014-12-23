b2fb743db7ade611f2dda7e82f04beef275e18cd
Dave Bell <bellfazar@gmail.com>
2014-12-23 14:52:37 -0500

    Initial commit

---
+++ VoiceControl/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.AccessControl;
+using System.Windows;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace VoiceControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            NotifyIcon ni = new NotifyIcon { Icon = Properties.Resources.Main, Visible = true };
+            ni.DoubleClick += new System.EventHandler(this.ni_DoubleClick);
+        }
+
+        private void ni_DoubleClick(object Sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = WindowState.Normal;
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+                this.Hide();
+
+            base.OnStateChanged(e);
+        }
+
+    }
+}
